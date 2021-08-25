7f167c045810608c1a992623a794d78b4af6fa7f
jiangchao <jiangchao@matech.cn>
2021-08-25 17:49:04 +0800

    提交角色能量储存判断

--- src/component/role/AbstractRole.ts
+++ src/component/role/AbstractRole.ts
@@ -1,6 +1,17 @@
 abstract class Role {
+  /**
+   * 角色
+   */
   public creep: Creep;
-    public constructor(creep: Creep) { this.creep = creep }
+  /**
+   *
+   * 角色构造函数,该函数完成角色初始化功能
+   * @param creep 需要初始化的角色
+   */
+  public constructor(creep: Creep) { this.creep = creep }
+  /**
+   * 角色行为
+   */
     public abstract run():boolean|void;
 
 }
--- src/component/role/haverster.ts
+++ src/component/role/haverster.ts
@@ -1,6 +1,15 @@
 export default class Havster extends Role{
     public run(): boolean | void {
-        throw new Error("Method not implemented.");
+        /**
+         * 判断该角色存储能量
+         */
+         if(this.creep.store.getFreeCapacity()< this.creep.store.getCapacity()) {
+            this.goHarvest(this.creep);
+        }
+    }
+    goHarvest(creep: Creep) {
+        throw new Error("Function not implemented.");
     }
 
 }
+
