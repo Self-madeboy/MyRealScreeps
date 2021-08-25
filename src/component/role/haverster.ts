ef22e0ad759a9852fd9789d72ce30358e58c8db5
jiangchao <jiangchao@matech.cn>
2021-08-25 17:51:31 +0800

    微调判断

--- src/component/role/haverster.ts
+++ src/component/role/haverster.ts
@@ -1,15 +1,13 @@
-export default class Havster extends Role{
-    public run(): boolean | void {
-        /**
-         * 判断该角色存储能量
-         */
-         if(this.creep.store.getFreeCapacity()< this.creep.store.getCapacity()) {
-            this.goHarvest(this.creep);
-        }
+export default class Havster extends Role {
+  public run(): boolean | void {
+    /**
+     * 判断该角色存储能量
+     */
+    if (this.creep.store.getFreeCapacity() == this.creep.store.getCapacity()) {
+      this.goHarvest(this.creep);
     }
-    goHarvest(creep: Creep) {
-        throw new Error("Function not implemented.");
-    }
-
+  }
+  goHarvest(creep: Creep) {
+    throw new Error("Function not implemented.");
+  }
 }
-
